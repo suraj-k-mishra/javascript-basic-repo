2ec6dc24ea824451a8e333c866c7ff45927bc0a0
suraj-k-mishra <surajmishra17799@gmail.com>
2022-01-20 09:07:29 +0530

    Javascript Basic concept file  Added

---
+++ hoisting/hoisting.js
@@ -0,0 +1,25 @@
+console.log(a)
+
+var a =10;
+b()
+
+function b(){
+    console.log('Hello b')
+}
+
+var x =10;
+var x;
+console.log(x)
+
+// capture varibale keep in momeber aasigna  default value called undefined
+// a=undefined
+
+// function b(){
+
+// }
+
+// x = undefined
+// x = undefined 
+
+// x=10
+// console.log(10)
